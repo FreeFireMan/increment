8205044716816321cca986da67a22483ce16a0f1
FreeFireMan <beluh.c.s@gmail.com>
2019-08-17 18:35:41 +0300

    increment v2

--- src/App.js
+++ src/App.js
@@ -6,35 +6,23 @@ class App extends React.Component{
   state = {
     count: 0,
   };
-  resultObserver = (it) =>{
-    this.setState(prev =>(
-        prev.count+it >= 0 ? { count: this.state.count+it} : alert("don't minus, do plus")
-    ))
-  }
-  plusMinusHandler = (e) => {
-    e.target.value.toString() === "Plus" ? this.resultObserver(1) :
-        this.resultObserver(-1)
-  }
 
-  resetHandler =(e) =>{
-    this.setState({
-      count: 0,
-    })
-  };
-  plusMinus100Handler = (e) => {
-    e.target.value.toString() === "Plus" ? this.resultObserver(100) :
-        this.resultObserver(-100)
-  }
+    countHandler = val =>
+      this.setState( pevState => ({
+          count: pevState.count + val >= 0 ? pevState.count + val : 0
+          })
+      )
 
   render() {
+        const {count} = this.state;
     return (
         <div className="App">
           <div><h1>{this.state.count}</h1></div>
-          <button onClick={this.plusMinusHandler} value="Plus" >Plus</button>
-          <button onClick={this.plusMinusHandler} >Minus</button>
-          <button onClick={this.resetHandler}>Reset</button>
-          <button onClick={this.plusMinus100Handler} value="Plus">Plus 100</button>
-          <button onClick={this.plusMinus100Handler}>Minus 100</button>
+          <button onClick={this.countHandler.bind(this,1)} value="Plus" >Plus</button>
+          <button onClick={this.countHandler.bind(this,-1)}>Minus</button>
+          <button onClick={this.countHandler.bind(this,-count)}>Reset</button>
+          <button onClick={this.countHandler.bind(this,100)}>Plus 100</button>
+          <button onClick={this.countHandler.bind(this,-100)}>Minus 100</button>
         </div>
     );
   }
